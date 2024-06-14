8e620d99b07fd7da0c6faf0035b3dcb77379fcd5
ref1o <fiorfepsn@gmail.com>
2024-06-14 04:02:45 +0200

    home, absolute and relative paths

--- app/main.py
+++ app/main.py
@@ -3,39 +3,40 @@ import shlex
 import os
 import subprocess
 
+from typing import Optional
+
 COMMANDS = ["cd", "pwd", "exit", "echo", "type"]
 PATH = os.environ.get("PATH")
 
+def locate_executable(file) -> Optional[str]:
+    for dir in PATH.split(":"):
+        path = os.path.join(dir, file)
+        if os.path.isfile(path) and os.access(path, os.X_OK):
+            return path
+
 def handle_exit(args):
-    if len(args) > 2:
-        print("invalid arguments")
-    elif len(args) == 1:
-        sys.exit(0)
-    else:
-        try:
-            sys.exit(int(args[1]))
-        except ValueError:
-            print("invalid exit code")
+    sys.exit(int(args[0]) if args else 0)
 
 def handle_echo(args):
-    if len(args) == 1:
-        print()
-    else:
-        print(" ".join(args[1:]))
+    print(" ".join(args))
         
 def handle_type(args):
-    cmd = args[1]
-    dirs = PATH.split(":")
-    print_string = f"{cmd}: not found"
+    if args[0] in builtins:
+        print(f"{args[0]} is a shell builtin")
+    elif path := locate_executable(args[0]):
+        print(f"{args[0]} is {path}")
+    else:
+        print(f"{args[0]}: not found")
+        
+def handle_pwd(args):
+    print(os.getcwd())
     
-    for dir in dirs:
-        path = f"{dir}/{cmd}"
-        if os.path.isfile(path):
-            print_string = f"{cmd} is {path}"
-            
-    if cmd in COMMANDS:
-        print_string = f"{cmd} is a shell builtin"
-    print(print_string)
+def handle_cd(args):
+    if args:
+        try:
+            os.chdir(args[0] if args[0] != "~" else os.environ.get("HOME", ""))
+        except FileNotFoundError:
+            print(f"cd: {args[0]}: No such file or directory")
     
 def find_file(file):
     dirs = PATH.split(":")
@@ -47,6 +48,14 @@ def find_file(file):
 
 def parse_input(input):
     return shlex.split(input)
+
+builtins = {
+    "exit": handle_exit,
+    "echo": handle_echo,
+    "type": handle_type,
+    "cd": handle_cd,
+    "pwd": handle_pwd,
+}
 
 def main():
     while True:
@@ -60,26 +69,16 @@ def main():
         # parse input
         args = parse_input(std_input)
         command = args[0]
-        path = find_file(command)
         
-        if len(args) == 0:
+        if command in builtins:
+            builtins[command](args[1:])
             continue
-        
-        if command == "exit":
-            handle_exit(args)
-            
-        elif command == "echo":
-            handle_echo(args)
-            
-        elif command == "type":
-            handle_type(args)
-            
-        elif path != "":
-            result = subprocess.run(args, capture_output=True, text=True)
-            print(result.stdout, end="")
-            
-        elif command not in COMMANDS:
+        elif path := locate_executable(command):
+            subprocess.run([path, *args[1:]])
+        else:
             print(f"{command}: command not found")
+        
+        sys.stdout.flush()
 
 if __name__ == "__main__":
     main()
